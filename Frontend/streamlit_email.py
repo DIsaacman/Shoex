8e4700e2df8ceace513c63b112534d4c801f8deb
MichaelNguyen2692 <107117536+MichaelNguyen2692@users.noreply.github.com>
2022-11-23 19:30:38 +1100

    Streamlit code to collect users' emails
    Collect users' names and email addresses.
    Store as a dataframe.
    Export dataframe into csv.

---
+++ Frontend/streamlit_email.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import csv
+
+st.write("# Collecting user email address")
+
+user_name = st.text_input("Please enter your name")
+
+user_email = st.text_input("Please enter your email address")
+
+if st.button("Display the user record"):
+    st.write(f"User name is {user_name}")
+    st.write(f"User email address is {user_email}")
+
+#create a dataframe 
+d = {'user_name': [user_name], 'user_email': [user_email]}
+df= pd.DataFrame(data=d)
+
+# Display the dataframe to streamlit
+st.write(df)
+
+#Export dataframe to designated folder
+df.to_csv(r'C:\Users\micha\OneDrive\Desktop\project3\useremail.csv')
